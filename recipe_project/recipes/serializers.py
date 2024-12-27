7509ac13f38dc748f3396607e5215ae1dc0bccf2
Yvonne <81848589+y123-cmd@users.noreply.github.com>
2024-12-27 21:11:39 +0300

    Updated database and serializers, added new Banana Cake recipe

--- recipe_project/recipes/serializers.py
+++ recipe_project/recipes/serializers.py
@@ -13,7 +13,15 @@ class UserSerializer(serializers.ModelSerializer):
 
 class RecipeSerializer(serializers.ModelSerializer):
     user = serializers.ReadOnlyField(source='user.username')
+    ingredients = serializers.ListField(
+        child=serializers.CharField(max_length=255)  # Each ingredient as a string
+    )
 
     class Meta:
         model = Recipe
         fields = '__all__'
+
+    def create(self, validated_data):
+        user = self.context['request'].user  # Get the authenticated user
+        validated_data['user'] = user  # Assign the authenticated user to the recipe
+        return super().create(validated_data)
